a032e3f31e5ca5cf4380e8a2f9a587cc49a14ec0
sarita <sarita.jaiswal@capgemini.com>
2020-12-20 19:16:36 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{ Component } from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+   constructor(props){
+     super(props);
+     this.state={
+       items: [],
+       isLoaded: false,
+     }
+   }
+   componentDidMount(){
+    let items= [];
+      fetch('http://localhost:8080/api/catchViolatedVehicles?currentTrafficLocation=NH road',{
+        method: 'POST'})
+      .then(res=>res.json())
+      .then(data=>{
+           this.setState({
+             isLoaded: true,
+             items: data,
+           })
+      });
+  }
+  checkViolations() {
+    alert("Vehicles with Violations!");
+  }
+  render(){
+     
+    var { isLoaded, items } = this.state;
+    if(!isLoaded){
+      return <div>Loading...</div>;
+    }
+    else{
+      return (
+         <div className="App">
+           <style>{'body { background-color: green; }'}</style>
+           <form>
+      </form>
+      <button onClick={this.checkviolations}><br></br>
+          <ul>
+          <p1 style={{color: "red"}}> Message: {items.message}<br></br></p1>
+          <p1 style={{color: "red"}}> Status: {items.status}</p1>
+          </ul>
+          </button>
+         </div>
+      );
+    }
+  }  
 }
-
+ReactDOM.render(<App />, document.getElementById('root'));
 export default App;
